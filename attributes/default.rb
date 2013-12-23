2ba3c32f67ba933a9f364d8f92dfd504b870aca9
William Bert <william@spanishdict.com>
2013-12-23 11:43:50 -0500

    Set default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['dotfiles'] = {
   :files => [], # Customize per user.
-  :standard_repository => nil, # Customize per organization.
-  :enabled_standard => false, # Customize per organization.
+  :standard_repository => 'https://github.com/spanishdict/dotfiles', # Customize per organization.
+  :enabled_standard => true, # Customize per organization.
   :data_bag => 'users', # Customize per organization.
   :custom_dotfiles_dir => '.custom_dotfiles', # Customizer per user.
   :custom_dotfiles_repo_prefix => nil # Customize per user.
